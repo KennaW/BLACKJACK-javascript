351a0321025eba109d425753325cf4bc23dcc580
Cynthia Wilhelm & kenna warsinske <student@epicodus.com>
2015-04-16 14:48:18 -0700

    initial logic to generate cards

--- js/script.js
+++ js/script.js
@@ -1,24 +1,59 @@
 var Hand = {
-  player: 0,
   score: 0,
   cards: []
 }
 
 var PlayingCard = {
-  face: "",
+  num: 0,
   point: 0,
-  suit: ""
+  suit: "",
+  face: function() {
+    if (this.num === 11) {
+      return "J";
+    } else if (this.num === 12) {
+      return "Q";
+    } else if (this.num === 13) {
+      return "K";
+    } else if (this.num === 1) {
+      return "A";
+    } else {
+      return "'" + this.num + "'";
+    }
+  }
 }
 
 var suit = [
   "H", "C", "S", "D"
-]
+];
+
 
 $(document).ready(function() {
 
-  var cardValue = Math.floor((Math.random() * 13) + 1);
-  var cardSuit = suit[Math.floor(Math.random()*suit.length)];
-  debugger;
+  $("#new-game").click(function() {
+    var human = Object.create(Hand);
+    var computer = Object.create(Hand);
+
+
+    $("#new-game").hide();
+    $("#hit").show();
+    $("#stand").show();
+
+    var card = Object.create(PlayingCard);
+    var newNum = Math.floor((Math.random() * 13) + 1);
+    var newSuit = suit[Math.floor(Math.random()*4)];
+    card.suit = newSuit;
+    card.num = newNum;
+    human.cards.push(card);
+
+    debugger;
+
+  });
+
+
+
+
+
+
 
 
 });
